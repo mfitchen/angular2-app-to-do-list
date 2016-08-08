428684b4527040fadd988c6bcfa443c70d32c651
Cory Olson and Marty Fitchen <cory@>
2016-08-08 16:42:41 -0700

    finish pipe and styling

--- app/task-list.component.ts
+++ app/task-list.component.ts
@@ -3,14 +3,21 @@ import { TaskComponent } from './task.component';
 import { Task } from './task.model';
 import { EditTaskDetailsComponent } from './edit-task-details.component';
 import { NewTaskComponent } from './new-task.component';
+import { DonePipe } from './done.pipe'
 
 @Component({
   selector: 'task-list',
   inputs: ['taskList'],
   outputs: ['onTaskSelect'],
+  pipes: [DonePipe],
   directives: [TaskComponent, EditTaskDetailsComponent, NewTaskComponent],
   template: `
-  <task-display *ngFor="#currentTask of taskList"
+  <select (change)="onChange($event.target.value)" class="filter" class="col-sm-3 form-control input-lg">
+    <option value="all">Show All</option>
+    <option value="done">Show Done</option>
+    <option value="notDone" selected="selected">Show Not Done</option>
+  </select>
+  <task-display *ngFor="#currentTask of taskList | done:filterDone"
       (click)="taskClicked(currentTask)"
       [class.selected]="currentTask === selectedTask"
       [task]="currentTask">
@@ -25,6 +32,7 @@ export class TaskListComponent {
   public taskList: Task[];
   public onTaskSelect: EventEmitter<Task>;
   public selectedTask: Task;
+  public filterDone: string = "notDone";
   constructor() {
     this.onTaskSelect = new EventEmitter();
   }
@@ -32,10 +40,15 @@ export class TaskListComponent {
     console.log("Child", clickedTask);
     this.selectedTask = clickedTask;
     this.onTaskSelect.emit(clickedTask);
-  }  
+  }
   createTask(description: string): void {
     this.taskList.push(
       new Task(description, this.taskList.length)
     );
+  }
+
+  onChange(filterOption){
+    this.filterDone = filterOption;
+    console.log(this.filterDone)
   }
 }
