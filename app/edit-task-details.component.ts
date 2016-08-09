e99079b276f622ce53b62aef075c022a2b2dc78f
Cory Olson and Marty Fitchen <cory@>
2016-08-09 11:49:27 -0700

    edit category and priority of existing objects

--- app/edit-task-details.component.ts
+++ app/edit-task-details.component.ts
@@ -8,7 +8,23 @@ import { Task } from './task.model'
     <div class="task-form">
       <h3>Edit Description:</h3>
       <input [(ngModel)]="task.description" class="col-sm-8 input-lg"/>
+      <div class="col-sm-2">
+        <select [(ngModel)]="task.priority" class="form-control input-lg">
+          <option value="high">High</option>
+          <option value="normal">Normal</option>
+          <option value="low">Low</option>
+        </select>
+      </div>
+      <div class="col-sm-2">
+        <select [(ngModel)]="task.category" class="form-control input-lg">
+          <option value="work">Work</option>
+          <option value="hobby">Hobby</option>
+          <option value="home">Home</option>
+        </select>
+      </div>
     </div>
+    <br>
+    <button (click)="addTask(newDescription, newPriority, newCategory)" class="btn-success btn-lg add-button">Add</button>
   `
 })
 
